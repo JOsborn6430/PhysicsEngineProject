7203f25d1c2c3e0acaaef9615ef28e4ccd3de47f
JOsborn6430 <JOSBORN6430@smail.pcd.edu>
2023-11-09 17:18:18 -0800

    Cleaned up Main
    I moved all vector operation methods to the new Op class. I would like the Main method to consist solely of the main physics update loop and collision detection. I haven't decided if we should include graphic update methods here or put them in their own specific class, or have each physics object contain its own graphic update methods.

--- PhysicsEngine/src/Main.java
+++ PhysicsEngine/src/Main.java
@@ -11,41 +11,8 @@ public class Main {
         b1.position = new double[]{100, 200};
         b1.momentum[0] = 1;
         b1.momentum[1] = 2;
-        printArrayD(b1.position);
-
-        b1.position = vectorAdditionD(b1.position, b1.momentum);
-
-        printArrayD(b1.position);
-
-        double[] errortest = {7,2};
-        printArrayD(vectorAdditionD(errortest, b1.momentum));
-        printArrayD(errortest);
-        printArrayD(scalarMultiplyD(errortest, 2.0));
-
-    }
-
-    public static double[] vectorAdditionD(double[] v1, double[] v2) {
-        if (v1.length != v2.length) {
-            System.out.println("Error: Vector dimensions do not match");
-            return new double[]{0};
-        } else for (int i = 0; i < v1.length; i++) {
-            v1[i] += v2[i];
-        }
-        return v1;
-    }
-
-    public static double[] scalarMultiplyD(double[] v, double a) {
-        for (int i = 0; i < v.length; i++) v[i] *= a;
-        return v;
-    }
-
-    public static void printArrayD(double[] array) {
-        System.out.print("[");
-        for (int i = 0; i < array.length; i++) {
-            System.out.print(array[i]);
-            if (i == array.length-1) break;
-            System.out.print(", ");
-        }
-        System.out.println("]");
+        Op.printArrayD(b1.position);
+        Op.printArrayD(Op.scalarMultiplyD(b1.position,3.0));
+        Op.printArrayD(b1.position);
     }
 }
